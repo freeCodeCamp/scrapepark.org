b0f1bb8661f7ed3015c0cd0ca490a5b5f96aa760
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2022-08-25 11:28:51 -0400

    fix: remove Google Map from contact pages
    This commit removes the Google Map that came with the template.
    It also removes JavaScript code related to this map.

--- js/custom.js
+++ js/custom.js
@@ -33,14 +33,3 @@ $(".client_owl-carousel").owlCarousel({
         }
     }
 });
-
-
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
